14b3b24a53c76f5aa518946e53f60c10b5f5f3c0
unknown <kc80943@gmail.com>
2023-03-02 14:57:49 +0700

    up code

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpSpeed = 5f; // Vận tốc nhảy
+    public float g = 9.8f; // Gia tốc trọng trường
+    private float verticalVelocity = 0f; // Vận tốc theo trục y
+
+    [SerializeField]
+    AudioClip clip;
+    [SerializeField]
+    AudioSource audio;
+
+
+    void Update()
+    {
+      
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            audio.clip = clip;
+            audio.Play();
+            verticalVelocity = jumpSpeed;
+            transform.rotation = Quaternion.Euler(0, 30, 0);
+        }
+        else
+        {
+            verticalVelocity -= g * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, -30, 0);
+        }
+
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y + verticalVelocity * Time.deltaTime,-3,5)); // Cập nhật vị trí của đối tượng
+    }
+
+
+}
---
+++ Assets/Scripts/backgound/backgoundMoving.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class backgoundMoving : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    Vector2 temp;
+    [SerializeField]
+    bool checkChimney = false;
+    float i;
+
+    [SerializeField]
+    AudioClip clip, clipD;
+    [SerializeField]
+    AudioSource audio;
+
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    Transform player;
+
+    int count = 0;
+    bool Checkcore = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+        Conllider();
+
+
+
+    }
+
+    void Moving()
+    {
+        temp = transform.localPosition;
+        temp.x = temp.x - speed;
+        if (temp.x <= -16)
+        {
+            transform.localPosition = new Vector2(18, 0);
+            RandomChimney();
+        }
+        else
+        {
+            transform.localPosition = temp;
+        }
+
+    }
+    void RandomChimney()
+    {
+        if (checkChimney)
+        {
+            i = Random.Range(3.0f, -1.0f);
+            Vector2 pos = transform.localPosition;
+            pos.y = i;
+            transform.localPosition = pos;
+
+
+        }
+
+    }
+
+    void Conllider()
+    {
+        if (player)
+        {
+            if (transform.localPosition.x <= -8 && transform.localPosition.x >= -10)
+            {
+                Debug.Log("đi qua x");
+                if (player.localPosition.y > transform.localPosition.y - 1.2f && player.localPosition.y < transform.localPosition.y + 1.2f)
+                {
+                    Debug.Log("đi qua y");
+                   
+                    if (text ) 
+                    {
+                        audio.clip = clip;
+                        audio.Play();
+                       
+
+                    }
+
+                }
+                else
+                {
+                    audio.clip = clipD;
+                    audio.Play();
+                    Time.timeScale = 0;
+             
+                }
+
+            }
+
+            if (player.localPosition.y < -2.6f && player.localPosition.y > 4.6f)
+            {
+                Time.timeScale = 0;
+                audio.clip = clipD;
+                audio.Play();
+               
+
+            }
+
+
+
+        }
+    }
+
+}
